0720e036331bc9420330b2273a22b42f5d3fdc1e
Martin Vanbrabant <martin.vanbrabant@ugent.be>
2021-06-22 09:52:46 +0200

    narrower file selection

--- utils/join-xyzt.sh
+++ utils/join-xyzt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# A utility to join files in a directory that have a '*.????.*' pattern in their name.
-# The joint output is in a file named '*.*', i.e. the '.????' portion is deleted
+# A utility to join files in a directory that have a '*.txt.????.txt' pattern in their name.
+# The joined output is in files named '*.txt.txt', i.e. the '.????' portion is deleted
 #
 # Parameters
 #   $1: input directory (default: .)
@@ -13,7 +13,7 @@ if [[ ! -d ${INDIR} ]] ; then echo "Input directory ${INDIR} does not exist!" ; 
 mkdir -p ${OUTDIR}
 if [[ ! -d ${OUTDIR} ]] ; then echo "Adapt permissions to allow creation of ${OUTDIR} and come back!" ; exit 2 ; fi
 
-find ${INDIR} -maxdepth 1 -type f -iname '*.????.*' | sort | while read f ; do
+find ${INDIR} -maxdepth 1 -type f -iname '*.txt.????.txt' | sort | while read f ; do
   echo $f
   base=${f##*/}
   outfile=$(echo $base | sed 's/\(.*\)\.....\(.*\)/\1\2/g')
